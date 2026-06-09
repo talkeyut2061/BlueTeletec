65d0f5d7c50e1756f641498dda8b3fdfccf96dc1
takey23541645 <takey23541645@icloud.com>
2026-06-09 18:46:16 +0900

    playerと武器の管理

---
+++ My project/Assets/Script/Player.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+/// <summary>
+/// playerの管理を行う
+/// </summary>
+public class Player : MonoBehaviour
+{
+
+    public List<GameObject> ItemSlot = new List<GameObject>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void Attack()
+    {
+        if (ItemSlot != null && Input.GetMouseButton(0))
+        {
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Weapon") && Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            ItemSlot.Add(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
