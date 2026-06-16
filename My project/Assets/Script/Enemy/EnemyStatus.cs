2f72d954063be88b9fb51b89231bdbc4c678ac94
takey23541645 <takey23541645@icloud.com>
2026-06-16 19:54:36 +0900

    何かと修正(まだ不安定)

--- My project/Assets/Script/Enemy/EnemyStatus.cs
+++ My project/Assets/Script/Enemy/EnemyStatus.cs
@@ -1,103 +1,132 @@
-﻿using JetBrains.Annotations;
-using KevinIglesias;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EnemyStatus : MonoBehaviour
 {
-
     [Header("main status")]
-    [SerializeField] int maxhitpoint = 100;
-    int hitpoint;
-    [SerializeField] public int attack = 0;
-    [SerializeField] int defense = 0;
-
+    [SerializeField] private int _maxhitpoint = 100;
+    private int _hitpoint;
+    [SerializeField] public int _enemyattack = 5;
+    [SerializeField] private int _defense = 5;
+    [SerializeField] private float _movespeed = 3f;
 
     [Header("Other")]
-    float distance;
-    Transform enemy;
-    Transform player;
-    HumanSoldierController controller;
+    [SerializeField] private float _distance;
+    [SerializeField] private Transform _enemy;
+    [SerializeField] private Transform player;
 
-    bool _isattack = false;
-    bool _isBattleRun = false;
+    bool _isAttack = true;
+    bool _isBattleRun = true;
 
     Animator _animator;
-    Player _player;
+    Animation _animation;
+    CharacterController _characterController;
+    WeaponSystem _weaponSystem;
+    WeaponObject _weaponObject;
 
     void Start()
     {
-        distance = Vector3.Distance(enemy.position, player.position);
-        hitpoint = maxhitpoint;
-        controller = GetComponent<HumanSoldierController>();
+        // 先に GetComponent しないと null になる
+        _weaponSystem = GetComponent<WeaponSystem>();
+        _weaponObject = GetComponent<WeaponObject>();
+
+        // 武器の攻撃力を加算
+        _enemyattack += _weaponObject.BasicAttack / 5;
+
+        _isAttack = false;
+        _isBattleRun = false;
+        _hitpoint = _maxhitpoint;
+
         _animator = GetComponent<Animator>();
-        _player = GetComponent<Player>();
+        _animation = GetComponent<Animation>();
+        _characterController = GetComponent<CharacterController>();
     }
 
-
-    /// <summary>
-    /// もし、enemyとplayerとの距離が5f(5m)以下の場合
-    /// _isattack flagをtrueかつ_isbattleRun flagがfalseの時のみ
-    /// enemyの視点をplayerに向けて、コルーチンを呼び出す。
-    /// </summary>
     void Update()
     {
-        if (distance <= 5f)
-        {
-            _isattack = !_isattack;
+        _distance = Vector3.Distance(_enemy.position, player.position);
 
-            if (_isattack && !_isBattleRun)
-                StartCoroutine(IsBattle());
+        // 30m以上 → 攻撃解除
+        if (_distance > 30f)
+        {
+            _isAttack = false;
+            _isBattleRun = false;
+            return;
+        }
+
+        // 20〜30m → 気付いて走る
+        if (_distance > 20f)
+        {
+            Debug.Log("enemyに気付かれた！ 逃げるか戦え！");
+            _isAttack = true;
+            _isBattleRun = true;
+            _animator.Play("Run", 3);
+
+            // ★ enemy が player の方向を向く（正しい書き方）
+            transform.forward = (player.position - transform.position).normalized * _movespeed;
+
+            return;
+        }
+
+        // 20m以内 → 攻撃モード
+        if (_distance <= 20f)
+        {
+            Debug.Log("enemyが攻撃する！ 上手く避けろ！");
+            _animator.SetTrigger("Walk");
+            StartCoroutine(IsBattle());
         }
     }
 
     IEnumerator IsBattle()
     {
-        _isBattleRun = true;
-
-        while (_isattack)
+        while (_isAttack)
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(3f);
 
             int random = Random.Range(0, 100);
 
             if (random >= 50)
             {
-                Debug.Log($"生成した乱数: {random}");
+                Debug.Log($"生成した乱数は {random}");
+                Debug.Log("enemyが攻撃した");
                 _animator.SetTrigger("Attack");
             }
             else
             {
-                Debug.Log($"生成した乱数: {random}");
+                Debug.Log($"生成した乱数は {random}");
+                Debug.Log("攻撃しなかった");
             }
         }
 
         _isBattleRun = false;
     }
 
-    public void TakeDamage(int amount)
+    private void OnCollisionEnter(Collision collision)
     {
-        //hitpoint -= amount;
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            //_hitpoint -= _player._attack - _defense;
 
-        //if (hitpoint <= 0)
-        if (Keyboard.current.leftCtrlKey.wasPressedThisFrame)
-            hitpoint -= _player.attack;
+            if (_hitpoint <= 0)
+                StartCoroutine(Die());
+            else
+                _animator.SetTrigger("Damage");
+        }
 
+        IEnumerator Die()
+        {
+            _characterController.enabled = false;
+            _animator.enabled = false;
 
-        else
-            _animator.SetTrigger("Damage");
+            yield return new WaitForSeconds(5f);
 
-        if (hitpoint >= 0)
-            StartCoroutine(Die());
-    }
+            _isAttack = false;
+            _isBattleRun = false;
 
-   IEnumerator Die()
-    {
-        _animator.enabled = false;
-        yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(5f);
 
-        _animator.enabled = true;
-
+            _animator.enabled = true;
+        }
     }
 }
--- My project/Assets/Script/Player/Player.cs
+++ My project/Assets/Script/Player/Player.cs
@@ -1,98 +1,95 @@
 ﻿using Cinemachine;
-using NUnit.Framework;
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 /// <summary>
 /// playerの管理を行う
+/// playerはweaponオブジェクトに触れたら、hitpointを減らして、damage animationを再生する
+/// もし、hitpointが0になったら、animatorやplayerinput コンポーネントをfalseにする
 /// </summary>
 public class Player : MonoBehaviour
 {
     [Header("Main Status")]
-    int hitpoint;
-    public int maxhitpoint = 0;
-    public int attack = 0;
-    public int defeace = 0;
+    [SerializeField] private int _hitpoint = 0;
+    [SerializeField] private int _maxhitpoint = 10;
+    [SerializeField] public int _playerattack = 10;
+    [SerializeField] private int _defense = 10;
 
     [Header("Inventri")]
-    public List<GameObject> ItemSlot = new List<GameObject>();
+    [SerializeField] private List<GameObject> ItemSlot = new List<GameObject>();
 
     [Header("Other")]
-    [SerializeField] private Player player;
-    [SerializeField] GameObject[] ragdoll;
-
-    PlayerInput _playerinput;
-    ThirdPersonController _thirdPersonController;
-    CharacterController _characterController;
-    ICinemachineCamera _Cinemachine;
-    WeaponSystem _weaponSystem;
-    Animator _animator;
-    GameObject muzzle;
+    [SerializeField] private GameObject[] ragdoll;
+    [SerializeField] private int ragdolltime = 5;
+    [SerializeField] private PlayerInput _playerinput;
+    [SerializeField] private ThirdPersonController _thirdPersonController;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform muzzle;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private bool _isDead = false;
 
     [Header("refer")]
-    EnemyStatus _enemyStatus;
+    [SerializeField] private EnemyStatus _enemyStatus;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    /// <summary>
-    /// 最初のみragdoll オブジェクトをfalseにする
-    /// </summary>
     void Start()
     {
         _thirdPersonController = GetComponent<ThirdPersonController>();
         _playerinput = GetComponent<PlayerInput>();
-        _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
+        _hitpoint = _maxhitpoint;
 
-        hitpoint = maxhitpoint;
         foreach (var item in ragdoll)
-        {
             item.SetActive(false);
-        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Attack());
+        if (_isDead) return;
+
+        // -------------------------------
+        // 旧 Input System（エラー原因）
+        // if (Input.GetMouseButtonDown(0))
+        //     Attack();
+        // -------------------------------
+
+        // 新 Input System（正しい書き方）
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            Attack();
+
+        // こちらは新 Input System なので OK
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            Debug.Log("playerは10ダメージを受けた");
+            _animator.SetTrigger("Damage");
+            _hitpoint -= 10;
+        }
+
         Death();
     }
 
-
     /// <summary>
-    /// ItemSlot listがnullではないかつ左クリックした際、遠距離武器なら発射、
-    /// 近接武器は振る
-    /// もし。nullであった場合、
-    /// handattack animationを再生かつ一部のcolliderオブジェクトをtrueにする
-    /// また遠距離武器は弾数制限があり、0の時に、Rキーでaddしない限り、遠距離は攻撃不可かGキーで
-    /// weaponオブジェクトをremoveするしかない
-    /// 
+    /// 素手 or 武器攻撃
     /// </summary>
-    IEnumerator Attack()
+    private void Attack()
     {
-        yield return new WaitForSeconds(2f);
-
-        if (ItemSlot == null && Input.GetMouseButton(0))
+        if (ItemSlot.Count == 0)
         {
             _animator.SetTrigger("Attack");
             ragdoll[0].SetActive(true);
         }
-
-        yield return null;
+        else
+        {
+            _animator.SetTrigger("Attack");
+            Instantiate(bullet, muzzle.position, muzzle.rotation);
+        }
     }
 
-
     /// <summary>
-    /// weaponオブジェクトとplayerが接した際に、
-    /// weaponオブジェクトはitemslot listにaddする
-    /// また、enemyオブジェクトに触れた時は、enemystatusのattack変数を参照して、
-    /// hitpointを減少する
+    /// weaponオブジェクトとplayerが接した際に拾う
     /// </summary>
-    /// <param name="collision"></param>
     private void OnControllerColliderHit(ControllerColliderHit collision)
     {
         if (collision.gameObject.CompareTag("Weapon"))
@@ -100,57 +97,47 @@ public class Player : MonoBehaviour
             Debug.Log("weaponを拾った");
             ItemSlot.Add(collision.gameObject);
             collision.gameObject.SetActive(false);
-
         }
 
         else if (collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.LogWarning("enemyに触れた");
-            hitpoint -= _enemyStatus.attack;
-
-            //if (hitpoint < 0)
-            //    Death();
+            Debug.LogWarning("enemyに触れたので、体力が減った");
+            _enemyStatus = collision.gameObject.GetComponent<EnemyStatus>();
+            _hitpoint -= _enemyStatus._enemyattack;
         }
     }
 
-
     /// <summary>
-    /// playerのhitpointが0を下回ったら、GetCompornentしたCompornentをnullにして、
-    /// ragdoll listにあるcollider objectをforeachでtrueにする
+    /// playerのhitpointが0を下回ったら死亡処理
     /// </summary>
     private void Death()
     {
-        if (Keyboard.current.enterKey.wasPressedThisFrame)
-        {
-            _animator.enabled = false;
-            _characterController.enabled = false;
-            _thirdPersonController.enabled = false;
-            _playerinput.enabled = false;
+        if (_hitpoint > 0) return;
 
-            foreach (var item in ragdoll)
-            {
-                item.SetActive(true);
-            }
+        _isDead = true;
 
-            StartCoroutine(AfterDeath());
+        _animator.enabled = false;
+        _thirdPersonController.enabled = false;
+        _playerinput.enabled = false;
 
-            
-        }
+        foreach (var item in ragdoll)
+            item.SetActive(true);
+
+        StartCoroutine(AfterDeath());
     }
 
     IEnumerator AfterDeath()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(ragdolltime);
 
         foreach (var item in ragdoll)
-        {
             item.SetActive(false);
-        }
+
+        _hitpoint = _maxhitpoint;
         _animator.enabled = true;
-        _characterController.enabled =true;
         _thirdPersonController.enabled = true;
         _playerinput.enabled = true;
 
-       
+        _isDead = false;
     }
 }
