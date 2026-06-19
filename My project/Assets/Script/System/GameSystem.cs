a3158af9eb53eaabcd4ad0beef2bde5e997d2a71
takey23541645 <takey23541645@icloud.com>
2026-06-19 15:54:57 +0900

    Textオブジェクトやゲームシステムの一部仕様変更

--- My project/Assets/Script/System/GameSystem.cs
+++ My project/Assets/Script/System/GameSystem.cs
@@ -1,6 +1,7 @@
 ﻿using StarterAssets;
 using System;
 using System.Collections;
+using System.Runtime.Serialization;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -22,17 +23,18 @@ public class GameSystem : MonoBehaviour
     [SerializeField] private int _killCount = 0;      // 現在のキル数
     [SerializeField] private int _startTimer = 3;     // ゲーム開始前のカウントダウン
     [SerializeField] private int _gameTimer = 180;     // ゲーム時間
+    [SerializeField] private int _normacount;
 
     [Header("Text Component")]
-    [SerializeField] private TMP_Text _title;
+    [SerializeField] private GameObject _title;
     [SerializeField] private TMP_Text _timeText;
+    [SerializeField] private TMP_Text _normaText;
     [SerializeField] private TMP_Text _killCountText;
     [SerializeField] private TMP_Text _killLog;
-    [SerializeField] private TMP_Text _stageClear;
+    [SerializeField] private TMP_Text _complete;  
+    [SerializeField] private TMP_Text _failed;    
 
-    [Header("Button and Canvas Component")]
-    [SerializeField] private Button _startButton;
-    [SerializeField] private Button _creditsButton;
+    [Header("Canvas component")]
     [SerializeField] private RawImage _creditsImage;
 
     [Header("SpawnPoint")]
@@ -43,13 +45,24 @@ public class GameSystem : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject _enemy;
 
+    [Header("Background music Audio Component")]
+    [SerializeField] private AudioSource _audioSource;     //共通のaudiosource
+    [SerializeField] private AudioClip _titlemusicclip;    //タイトル画面で流れるsound
+    [SerializeField] private AudioClip _battlemusicclip;   //ゲーム中(戦闘中)に流れるbgm
+    [SerializeField] private AudioClip _losemusicclip;     //ノルマ未達成に流れる
+    [SerializeField] private AudioClip _gamewinsmusicclip; //ノルマ達成時に流れる
 
-    [Header("Audio Component")]
-    [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private AudioClip _titlemusicclip;
-    [SerializeField] private AudioClip _battlemusicclip;
-    [SerializeField] private AudioClip _clicksoundclip;
-    
+    [Header("Effect sound Audio Component")]
+    [SerializeField] private AudioClip _clicksoundclip;    //クリック音
+    [SerializeField] private AudioClip _damagevoiceclip;   //playerやnpcのダメージボイス
+    [SerializeField] private AudioClip _deathvoiceclip;    //playerやnpc(enemy)の死亡ボイス
+
+    void Awake()
+    {
+        _complete.gameObject.SetActive(false);
+        _failed.gameObject.SetActive(false);
+        _normaText.gameObject.SetActive(false);
+    }
 
     void Start()
     {
@@ -58,54 +71,80 @@ public class GameSystem : MonoBehaviour
         _timeText.enabled = false;
         _killCountText.enabled = false;
         _killLog.enabled = false;
-        _stageClear.enabled = false;
-        _title.enabled = true;
+        _title.SetActive(true);
         _creditsImage.gameObject.SetActive(false);
-
-        // ボタンにイベント登録
-        _startButton.onClick.AddListener(GameStart);
         _enemy.SetActive(false);
         _player.SetActive(false);
-
-        _audioSource.PlayOneShot(_titlemusicclip);
+       
+        // ★FIX: タイトルBGMを正しくループ再生する
+        _audioSource.clip = _titlemusicclip;
         _audioSource.loop = true;
+        _audioSource.Play();
+        // _audioSource.PlayOneShot(_titlemusicclip);
+        // _audioSource.loop = true;
+
+       
+    }
+
+    void Update()
+    {
+        // ★FIX: Credits 表示中だけ ESC キーでタイトルに戻る
+        if (_creditsImage.gameObject.activeSelf)
+        {
+            if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+            {
+                BackTitle();
+            }
+        }
     }
 
     /// <summary>
     /// buttonが押されたら、このメソッドを呼び出し、gameをstartする
     /// gametimerで時間を変更出来る
     /// </summary>
-    private void GameStart()
+    public void GameStart()
     {
+        _normacount = (_gameTimer / 6) - 5;
+
+        // ★ここで normaText を更新する
+        _normaText.text = $"{_gameTimer}秒以内に敵を{_normacount}体、倒せ！";
+
         _audioSource.PlayOneShot(_clicksoundclip);
         StartCoroutine(GameFlow());
     }
-
 
     /// <summary>
     /// クレジット出す用
     /// escキーで戻る
     /// </summary>
-    private void Credit()
+    public void Credit()
     {
         _audioSource.PlayOneShot(_clicksoundclip);
         _creditsImage.gameObject.SetActive(true);
-        _startButton.gameObject.SetActive(false);
-        _creditsButton.gameObject.SetActive(false);
-        _title.enabled = false;
+        _title.SetActive(false);
     }
 
-  
-        /// <summary>
-        /// ゲーム全体の流れを管理するコルーチン
-        /// </summary>
-        IEnumerator GameFlow()
+    public void BackTitle()
+    {
+        // ★FIX: Start() を呼び直さず、必要なオブジェクトだけ状態を戻す
+        _creditsImage.gameObject.SetActive(false);
+        _title.SetActive(true);
+    }
+
+    /// <summary>
+    /// ゲーム全体の流れを管理するコルーチン
+    /// </summary>
+    IEnumerator GameFlow()
     {
         // カウントダウン表示 
         _audioSource.Stop();
-        _startButton.gameObject.SetActive(false);
-        _title.enabled = false;
+        _title.SetActive(false);
+        _normaText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(2f);
+        _normaText.gameObject.SetActive(false);
         _timeText.enabled = true;
+
         for (int i = _startTimer; i > 0; i--)
         {
             _timeText.text = i.ToString();
@@ -117,14 +156,18 @@ public class GameSystem : MonoBehaviour
         _killCountText.enabled = true;
         _killCountText.text = "Kill : 0";
 
+        // ★FIX: バトルBGMを1回だけ再生してループ
+        _audioSource.clip = _battlemusicclip;
+        _audioSource.loop = true;
+        _audioSource.Play();
         // ゲームタイマー
         int timer = _gameTimer;
-        
 
-        while (timer > 0)
+        while (timer >= 0)
         {
             Cursor.visible = false;
-            _audioSource.PlayOneShot(_battlemusicclip);
+            // _audioSource.PlayOneShot(_battlemusicclip); // ← 毎秒鳴っていたバグ
+
             _player.SetActive(true);
             _enemy.SetActive(true);
             _timeText.text = timer.ToString();
@@ -140,7 +183,7 @@ public class GameSystem : MonoBehaviour
     /// KillCount を +1 し、ログを表示する
     /// EnemyStatus から呼び出す想定
     /// </summary>
-    private void AddKill()
+    public void AddKill() // ★FIX: 外部から呼ぶので public に
     {
         _killCount++;
         _killCountText.text = $"Kill : {_killCount}";
@@ -163,15 +206,19 @@ public class GameSystem : MonoBehaviour
     /// </summary>
     private void CheckClear()
     {
-        if (_killCount >= 10) // 例：10体倒せばクリア
+        if (_killCount >= _normacount) // 例：_normacount体倒せばクリア
         {
-            _stageClear.enabled = true;
-            _stageClear.text = "GAME CLEAR!";
+            _player.SetActive(false);
+            _complete.gameObject.SetActive(true);
+            _timeText.enabled = false;
+            _complete.text = "GAME CLEAR!";
         }
         else
         {
-            _stageClear.enabled = true;
-            _stageClear.text = "FAILED...";
+            _player.SetActive(false);
+            _failed.gameObject.SetActive(true);
+            _timeText.enabled = false;
+            _failed.text = "FAILED...";
         }
     }
 
